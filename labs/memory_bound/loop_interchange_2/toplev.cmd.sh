7983e0758f4f70e540b62e05dc9edc01e3dd3bd4
bi-an <2608120872@qq.com>
2025-11-22 22:41:24 +0800

    Add toplev.cmd.sh

---
+++ labs/memory_bound/loop_interchange_2/toplev.cmd.sh
@@ -0,0 +1,2 @@
+toplev --core S0-C0 -l2 --no-desc taskset -c 0 ./lab ../pexels-pixabay-434334.pbm output.pgm
+toplev --core S0-C0 -l2 --no-desc --run-sample taskset -c 0 ./lab ../pexels-pixabay-434334.pbm output.pgm

cc8fc9e77870d82b057c84df546130fb89c3d931
bi-an <2608120872@qq.com>
2025-11-24 22:51:09 +0800

    Solved loop_interchange_2

--- tools/toplev.cmd.sh
+++ tools/toplev.cmd.sh
@@ -1,2 +1,3 @@
-toplev --core S0-C0 -l2 --no-desc taskset -c 0 ./lab ../pexels-pixabay-434334.pbm output.pgm
-toplev --core S0-C0 -l2 --no-desc --run-sample taskset -c 0 ./lab ../pexels-pixabay-434334.pbm output.pgm
+toplev --core S0-C0 -l2 --no-desc taskset -c 0 ./lab
+toplev --core S0-C0 -l2 --no-desc --run-sample taskset -c 0 ./lab
+perf stat --topdown -a taskset -c 0 ./lab

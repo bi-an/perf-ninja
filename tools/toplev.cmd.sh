23d546f4f9021f3d8ad0421533b71f34ffdf9991
bi-an <2608120872@qq.com>
2025-11-27 19:55:25 +0800

    补充toplev.cmd.sh

--- tools/toplev.cmd.sh
+++ tools/toplev.cmd.sh
@@ -1,3 +1,5 @@
 toplev --core S0-C0 -l2 --no-desc taskset -c 0 ./lab
+toplev --core S0-C0 -l2 --no-desc -v taskset -c 0 ./lab
 toplev --core S0-C0 -l2 --no-desc --run-sample taskset -c 0 ./lab
+
 perf stat --topdown -a taskset -c 0 ./lab

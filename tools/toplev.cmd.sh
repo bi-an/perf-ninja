8a7f23e8f9bafae2cfbcf3d4a5531cb604910d48
bi-an <2608120872@qq.com>
2025-11-28 22:31:07 +0800

    toplev.cmd.sh补充perf -report -M Intel命令;vectorization_1/result.md补充序列对齐算法的解释

--- tools/toplev.cmd.sh
+++ tools/toplev.cmd.sh
@@ -3,3 +3,6 @@ toplev --core S0-C0 -l2 --no-desc -v taskset -c 0 ./lab
 toplev --core S0-C0 -l2 --no-desc --run-sample taskset -c 0 ./lab
 
 perf stat --topdown -a taskset -c 0 ./lab
+
+perf record ./lab
+perf report -n -M Intel

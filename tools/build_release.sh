af6c0bedb04f2529c4964e6cafca4be072234491
bi-an <2608120872@qq.com>
2025-11-30 15:10:41 +0800

    build*.sh增加-DCMAKE_EXPORT_COMPILE_COMMANDS选项

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -1,4 +1,4 @@
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build --config Release --parallel `nproc`
 cmake --build build --target validateLab
 cmake --build build --target benchmarkLab
